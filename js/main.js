d399bc969b637fb8aa9e0a032c7f7edcde71afa0
pkv1979 <ACMilan1979@gmail.com>
2020-08-07 09:10:04 +0300

    feat: task 2 completed

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ const sumTo = (n) => {
 };
 
 sumTo(5);
+
+// Задание №2
+// Как исправить код ниже, чтобы он вывел в консоль 5 разных значений (от 0 до 4):
+// for (var i = 0; i < 5; i++) {
+// setTimeout(function () {
+// console.log(i);
+// }, 1000);
+// }
+for (let i = 0; i < 5; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 1000);
+}
